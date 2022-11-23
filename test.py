090cc44a44835c9bf366d40ce48c2136be2ab99c
Drew Taylor <116771412+dtaylor338@users.noreply.github.com>
2022-11-22 21:43:23 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+#the test ran quickly and found no errors. The result means that this code was free from errors.
